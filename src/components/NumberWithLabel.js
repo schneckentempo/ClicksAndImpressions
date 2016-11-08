9f1bb55faa83cd0616a1465b4a7a6cfd9dcc69b5
schneckentempo <wild.matthias@gmx.net>
2016-11-08 11:51:06 +0100

    adding semicolon-rule + fixing

--- src/components/NumberWithLabel.js
+++ src/components/NumberWithLabel.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from 'react'
 
 export default function NumberWithLabel({ label, number }) {
   const outerSpanStyle = {
     marginRight: '1em',
     display: 'inline',
-  };
+  }
   const innerSpanStyle = {
     color: '#DD0000',
     backgroundColor: '#EEEEEE',
     border: '1px gray',
     borderRadius: '3px',
     padding: '0.3em',
-  };
+  }
 
   return (
     <span style={outerSpanStyle}>
       { label } <span style={innerSpanStyle}>{ number }</span>
     </span>
-);
+)
 }
 
 NumberWithLabel.propTypes = {
   label: React.PropTypes.string,
   number: React.PropTypes.number,
-};
+}
