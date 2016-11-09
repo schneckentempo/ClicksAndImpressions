56bd29ba483fe4673d911d11bb36f959ab7f2813
schneckentempo <wild.matthias@gmx.net>
2016-11-09 09:28:51 +0100

    test-example (actually not working)

--- src/test/test2.spec.js
+++ src/test/test2.spec.js
@@ -3,12 +3,12 @@ import React from 'react'
 import { expect } from 'chai'
 import { mount, shallow } from 'enzyme'
 import sinon from 'sinon'
-import HeaderText from '../components/HeaderText'
+import App from '../App'
 
-describe('<HeaderText />', () => {
+describe('<App />', () => {
   it('calls componentDidMount', () => {
-    sinon.spy(HeaderText.prototype, 'componentDidMount')
-    const wrapper = mount(<HeaderText />)
-    expect(HeaderText.prototype.componentDidMount.calledOnce).to.equal(true)
+    const spy = sinon.spy(App, 'componentDidMount')
+    const wrapper = mount(<App />)
+    expect(App.componentDidMount.calledOnce).to.equal(true)
   })
 })
