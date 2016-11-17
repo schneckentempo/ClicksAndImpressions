1db875b4f2179b8e43dec5858aa05b86ddc0b7de
schneckentempo <wild.matthias@gmx.net>
2016-11-17 16:46:08 +0100

    first version with redux for App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
 import CsvMappingApplier from './components/CsvMappingApplier'
 import SumNumbersForDimensionValueWidget from './components/SumNumbersForDimensionValueWidget'
 import csvToJson from './utils/csvToJson'
 import './styles.css'
 
-export default class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      csvData: '',
-      mapping: {},
-      normalizedCsv: [],
-      dimensionValues: [],
-    }
-  }
+// action name
+const APPLY_DATA = 'APPLY_DATA'
 
+// action creator
+const applyData = (csvData, mapping, normalizedCsv, dimensionValues) => (
+  {
+    type: APPLY_DATA,
+    csvData,
+    mapping,
+    normalizedCsv,
+    dimensionValues,
+  }
+)
+
+class App extends Component {
   handleApply = (csvData, mapping) => {
+    const { dispatch } = this.props
     const { dimensionValues, normalizedCsv } = csvToJson(csvData, mapping)
-    this.setState({ csvData, mapping, normalizedCsv, dimensionValues })
+
+    dispatch(applyData(csvData, mapping, normalizedCsv, dimensionValues))
   }
 
   render() {
-    const { normalizedCsv, dimensionValues, mapping } = this.state
+    const { normalizedCsv, dimensionValues, mapping } = this.props
     return (
       <div>
         <CsvMappingApplier defaultDataSource="http://mockbin.org/bin/ee7a13ae-4732-445d-ac76-27bc8e74edc5" onApply={this.handleApply} />
@@ -33,4 +40,19 @@ export default class App extends Component {
       </div>
     )
   }
+}
+
+const mapStateToProps = ({ normalizedCsv, dimensionValues, mapping }) => ({
+  normalizedCsv,
+  dimensionValues,
+  mapping,
+})
+
+export default connect(mapStateToProps)(App)
+
+App.propTypes = {
+  dispatch: PropTypes.func,
+  mapping: PropTypes.objectOf(PropTypes.array),
+  normalizedCsv: PropTypes.arrayOf(PropTypes.object),
+  dimensionValues: PropTypes.arrayOf(PropTypes.object),
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import React from 'react'
+import { createStore } from 'redux'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const initialState = {
+  csvData: '',
+  mapping: {},
+  normalizedCsv: [],
+  dimensionValues: [],
+}
+
+// reducer
+const dataReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'APPLY_DATA':
+      return {
+        csvData: action.csvData,
+        mapping: action.mapping,
+        normalizedCsv: action.normalizedCsv,
+        dimensionValues: action.dimensionValues,
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(dataReducer)
+
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'))
