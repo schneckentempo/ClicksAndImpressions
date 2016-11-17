846a2d3065fbe7997f7cb07bda9c0208dac3e06a
schneckentempo <wild.matthias@gmx.net>
2016-11-17 13:10:59 +0100

    fix onBlur and naming of util

--- src/components/CsvMappingApplier.js
+++ src/components/CsvMappingApplier.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import HeaderText from './HeaderText'
 import styles from './CsvMappingApplier.css'
-import getDataMapping from '../utils/getMappingFromDatasource'
+import getMappingFromDatasource from '../utils/getMappingFromDatasource'
 
 const axios = require('axios')
 
@@ -9,7 +9,7 @@ export default class CsvMappingApplier extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      mapping: JSON.stringify(getDataMapping(props.defaultDataSource), undefined, 2),
+      mapping: JSON.stringify(getMappingFromDatasource(props.defaultDataSource), undefined, 2),
       csvData: '',
       badRequest: false,
       badMapping: false,
@@ -51,7 +51,7 @@ export default class CsvMappingApplier extends Component {
 
     axios.get(dataSource).then((response) => {
       const csvData = response.data
-      const mapping = response.data !== '' ? JSON.stringify(getDataMapping(dataSource), undefined, 2) : '{}'
+      const mapping = response.data !== '' ? JSON.stringify(getMappingFromDatasource(dataSource), undefined, 2) : '{}'
 
       if (csvData !== '') {
         this.setState({ mapping, csvData, badRequest: false })
@@ -81,7 +81,7 @@ export default class CsvMappingApplier extends Component {
         <input
           ref={(ref) => { this.inputField = ref }}
           defaultValue={defaultDataSource}
-          onBlur={this.onBlur}
+          onBlur={this.onBlurInput}
           type="text"
           className={styles.sourceInput}
           style={badRequestStyle}
