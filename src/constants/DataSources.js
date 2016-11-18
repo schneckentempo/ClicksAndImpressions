b08aafee1972ec9ee59573645a07b148d02f87f7
schneckentempo <wild.matthias@gmx.net>
2016-11-18 13:16:03 +0100

    constants for datasources

---
+++ src/constants/DataSources.js
@@ -0,0 +1,2 @@
+export const DATA_SOURCE_1 = 'http://mockbin.org/bin/3f1037be-88f3-4e34-a8ec-d602779bf2d6'
+export const DATA_SOURCE_2 = 'http://mockbin.org/bin/ee7a13ae-4732-445d-ac76-27bc8e74edc5'
